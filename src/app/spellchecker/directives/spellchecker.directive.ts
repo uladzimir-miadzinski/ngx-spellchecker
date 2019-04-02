5d5556b03c470f349d2cc3ef352f54562d20b09d
Uladzimir.Miadzinski <uladzimir_miadzinski@epam.com>
2019-04-02 10:47:42 +0300

    ckeditor integration demo

--- src/app/spellchecker/directives/spellchecker.directive.ts
+++ src/app/spellchecker/directives/spellchecker.directive.ts
@@ -2,6 +2,7 @@ import {Directive, ElementRef, HostListener, Input, OnInit} from '@angular/core'
 import {SpellcheckerService} from '../../services/spellchecker.service';
 import {getCaretCharacterOffsetWithin, setCaretPosition} from '../helpers/caret';
 import {debounce} from '../helpers/delay';
+import {__await} from 'tslib';
 
 enum NodeType {
   Div = 1,
@@ -34,7 +35,7 @@ export class SpellcheckerDirective implements OnInit {
   
   get errorStyle() {
     return `
-    .spellchecker-error {
+    mark.tf-spellchecker-err {
       ${Object.entries(this.options.style).reduce((acc, [key, value]) => acc + `${key}:${value};`, '')}
     }
     `;
@@ -47,7 +48,7 @@ export class SpellcheckerDirective implements OnInit {
   options: SpellcheckerOptions = {
     style: {
       'text-decoration': 'none',
-      'background': '#f8d2d4',
+      'background': 'transparent', //'#f8d2d4',
       'border-bottom': '1px solid #e00',
       'box-shadow': 'inset 0 -1px 0 #e00',
       'color': 'inherit',
@@ -55,37 +56,19 @@ export class SpellcheckerDirective implements OnInit {
     }
   };
   
-  getWrapper(suggestions: string[] = []) {
-    const end = '</span>';
-    const start = `<span class="spellchecker-error" data-suggest="${suggestions}">`;
-    const length = end.length + start.length;
-    return {end, length, start};
-  }
-  
-  getWrappedWord(word: string, suggestions: string[]): string {
-    const wrapper = SpellcheckerDirective.getWrapper(suggestions);
-    return `${wrapper.start}${word}${wrapper.end}`;
-  }
-  
-  getIndexesOfWordInText(word: string = '', text: string = '') {
-    const fullWord = new RegExp(`\\b${word}\\b`, 'u');
-    return text.search(fullWord);
-  }
-  
-  getNodeValue(node: ChildNode) {
-    const el = node as HTMLElement;
+  getMatches(word, text) {
+    const regex = new RegExp(`\\b${word}\\b`, 'gi');
+    const result = [];
     
-    return (el.nodeType === NodeType.Div)
-      ? el.innerText
-      : el.nodeType === NodeType.Text
-        ? el.nodeValue
-        : el.textContent;
-  }
-  
-  getNodeWithMisspelledText(nodes: ChildNode[], word) {
-    return nodes.find((node: ChildNode) => {
-      return this.getIndexesOfWordInText(word, this.getNodeValue(node)) >= 0;
-    });
+    let match = null;
+    do {
+      match = regex.exec(text);
+      if (match) {
+        result.push(match.index);
+      }
+    } while (match);
+    
+    return result;
   }
   
   ngOnInit(): void {
@@ -102,97 +85,71 @@ export class SpellcheckerDirective implements OnInit {
     const el = this.el.nativeElement as HTMLInputElement;
     const textToSend = el.innerText || '';
     const response = await this.spellcheckService.checkText(textToSend);
-    
-    // avoid calc
-    if (el.innerText.length === textToSend.length) {
-      const caretOffset = getCaretCharacterOffsetWithin(el);
-      this.spellWordsHtml(response.misspelledWords, textToSend);
-      // this.spellWords(response.misspelledWords, textToSend);
-      // setCaretPosition(el, caretOffset);
-    }
+    await this.spellWordsHtml(response.misspelledWords, el);
   }
   
   
   @HostListener('contextmenu', ['$event'])
   onContextMenu(e: MouseEvent) {
-    const suggestions = (e.target as HTMLSpanElement).dataset.suggest.split(',');
-    const caretOffset = getCaretCharacterOffsetWithin((e.target as HTMLSpanElement));
-    console.log(caretOffset);
-    console.log(e);
-    console.log(suggestions);
-    console.log('contextmenu');
+    //const suggestions = (e.target as HTMLSpanElement).dataset.suggest.split(',');
   }
   
-  spellWordsHtml(misspelledWords, textWasSent) {
-    const el = this.el.nativeElement as HTMLInputElement;
-    const {childNodes} = el;
-    const nodes = Array.from(childNodes);
-    misspelledWords.forEach((misspelled: Misspelled) => {
-      const nodeWithMisspelledWord = this.getNodeWithMisspelledText(nodes, misspelled.word);
-      this.spellWord(misspelled.word, misspelled.suggestions, nodeWithMisspelledWord);
-      console.dir(nodeWithMisspelledWord);
-    });
-    console.log(childNodes);
+  async spellWordsHtml(misspelledWords, el: HTMLInputElement) {
+    await this.makeChildNodesWithSpelling(el.childNodes, misspelledWords);
   }
   
-  spellWord(word = '', suggestions = [], node) {
-    const nodeValue = this.getNodeValue(node);
-    const indexesOfWord = this.getIndexesOfWordInText(word, nodeValue);
-    const wrapper = this.getWrapper(suggestions);
-    const isWrappedBefore = this.getIndexesOfWordInText(`${wrapper.start}${word}`, nodeValue);
-    
-  }
-  
-  spellWords(misspelledWords, textWasSent) {
-    const el = this.el.nativeElement as HTMLInputElement;
-    const innerHtml = el.innerHTML;
-    console.log(innerHtml);
-    let text = innerHtml;
-    let outputText = '';
-    
-    if (misspelledWords.length) {
-      let textCursor = 0;
-      let wordCursor = 0;
-      
-      while (text.length > 0) {
-        // avoid calc
-        if (textWasSent.length !== el.innerText.length) {
-          return;
-        }
-        
-        textCursor = this.getIndexesOfWordInText(misspelledWords[wordCursor].word, text);
-        const children = Array.from(el.children);
-        const errorSpell = !!children.find(span => {
-          console.group('children span if');
-          console.dir(span);
-          console.dir((span as HTMLSpanElement).innerText);
-          console.dir(misspelledWords[wordCursor].word);
-          console.groupEnd();
-          return span.className === 'spellchecker-error' && this.getIndexesOfWordInText(misspelledWords[wordCursor].word, (span as HTMLSpanElement).innerText) >= 0;
-        });
-        
-        console.log(`errorSpell: ${errorSpell}`);
-        
-        if (textCursor >= 0 && !errorSpell) {
-          const spelledWord = this.getWrappedWord(misspelledWords[wordCursor].word, misspelledWords[wordCursor].suggestions);
-          outputText += `${text.slice(0, textCursor)}${spelledWord}`;
-          text = text.slice(textCursor + misspelledWords[wordCursor].word.length);
-        }
-        
-        wordCursor++;
-        if (wordCursor === misspelledWords.length) {
-          break;
-        }
-      }
-      
-      // `text` - contains rest of the text if there is no mistakes
-      // try to replace full text, if cant then user continue changing text
-      const newInnerHtml = outputText + text;
-      
-      // avoid inserting if user continue typing
-      if (el.innerText.length === textWasSent.length) {
-        el.innerHTML = newInnerHtml;
+  async makeChildNodesWithSpelling(childNodes, misspelledWords) {
+    for (let m = 0; m < misspelledWords.length; m++) {
+      for (let i = 0; i < childNodes.length; i++) {
+        await this.makeNodeWithSpelling(childNodes[i], misspelledWords[m]);
       }
     }
   }
+  
+  async makeNodeWithSpelling(node, misspelled) {
+    if (node.hasChildNodes()) {
+      if (node.nodeName === 'MARK' && node.textContent === node.dataset.misspelled) {
+        // value inside <mark> not changed
+        return;
+      } else if (node.nodeName === 'MARK') {
+        // value inside <mark> was changed, need to update <mark> node with new spell, or correct
+        const corrections = await this.spellcheckService.checkText(node.textContent);
+        if (corrections.length) {
+          // word with another errors
+        } else {
+          // word without errors
+          const caret = getCaretCharacterOffsetWithin(this.el.nativeElement as HTMLInputElement);
+          const textNodeFrag = document.createTextNode(node.textContent);
+          node.parentNode.replaceChild(textNodeFrag, node);
+          setCaretPosition(this.el.nativeElement as HTMLInputElement, caret);
+        }
+      }
+      await this.makeChildNodesWithSpelling(node.childNodes, [misspelled]);
+    } else if (node.nodeValue) {
+      // text value of node, the smallest part
+      
+      let newNodeValue = node.nodeValue;
+      const matches = this.getMatches(misspelled.word, node.nodeValue);
+      const mark = document.createElement('mark');
+      
+      mark.dataset.suggest = misspelled.suggestions;
+      mark.dataset.misspelled = misspelled.word;
+      mark.setAttribute('class', 'tf-spellchecker-err');
+      mark.innerHTML = misspelled.word;
+      
+      matches.forEach(matchIndex => {
+        newNodeValue = newNodeValue.slice(0, matchIndex) + mark.outerHTML + newNodeValue.slice(matchIndex + misspelled.word.length);
+      });
+      
+      const frag = document.createRange().createContextualFragment(newNodeValue);
+      const caret = getCaretCharacterOffsetWithin(this.el.nativeElement as HTMLInputElement);
+      
+      node.parentNode.replaceChild(frag, node);
+      
+      setCaretPosition(this.el.nativeElement as HTMLInputElement, caret);
+      
+      console.log('new nodeValue was set');
+    }
+  }
+  
 }
